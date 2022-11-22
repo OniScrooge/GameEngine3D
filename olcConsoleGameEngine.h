68ea65e2bc59114c61b0dd2b47dcc8061708d632
Patrick Iganut <puut4991@yahoo.com>
2022-11-22 16:42:13 -0600

    Slight edit to folder titles

--- olcConsoleGameEngine.h
+++ olcConsoleGameEngine.h
@@ -833,7 +833,7 @@ private:
 
 				// Update Title & Present Screen Buffer
 				wchar_t s[256];
-				swprintf_s(s, 256, L"OneLoneCoder.com - Console Game Engine - %s - FPS: %3.2f", m_sAppName.c_str(), 1.0f / fElapsedTime);
+				swprintf_s(s, 256, L" Console Game Engine - % s - FPS: % 3.2f", m_sAppName.c_str(), 1.0f / fElapsedTime);
 				SetConsoleTitle(s);
 				WriteConsoleOutput(m_hConsole, m_bufScreen, { (short)m_nScreenWidth, (short)m_nScreenHeight }, { 0,0 }, &m_rectWindow);
 			}
